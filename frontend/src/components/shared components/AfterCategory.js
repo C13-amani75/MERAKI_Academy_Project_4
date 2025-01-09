90109c869afd3bec27a8a40baa0485449830a31f
amani75 <amoonfathi1998@gmail.com>
2025-01-09 17:22:44 +0300

    un-completed

--- frontend/src/components/shared components/AfterCategory.js
+++ frontend/src/components/shared components/AfterCategory.js
@@ -1,18 +1,38 @@
 import React from 'react'
-
+import { SlBasket } from "react-icons/sl";
 const AfterCategory = () => {
   return (
     <div className='afterCaregory'>
+        <h2>Our Services</h2>
     <div className='.a123' id="carouselExampleControls" class="carousel slide" data-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img className='w123'  class="d-block w-100 " src="https://res.cloudinary.com/drhlmb3qr/image/upload/v1736263030/mqeha0nn4bopydp5jsp0.jpg" alt="First slide"/>
+    <p>Designing</p>
+      <img className='w123'  class="d-block w-100 " src="https://res.cloudinary.com/drhlmb3qr/image/upload/v1736428929/pexels-ron-lach-9849312_qudmsz.jpg" alt="First slide"/>
     </div>
     <div class="carousel-item">
-      <img className='w123'  class="d-block w-100 " src="https://res.cloudinary.com/drhlmb3qr/image/upload/v1736409533/Charlize_Theron_Has_a_Golden_Moment_at_the_CDGA_Awards_nn1kyh.jpg" alt="Second slide"/>
+    <p>Creating & Production</p>
+       <img className='w123'  class="d-block w-100 " src="https://res.cloudinary.com/drhlmb3qr/image/upload/v1736431254/pexels-michael-burrows-7147652_wmq4x9.jpg" alt="Second slide"/>
     </div>
     <div class="carousel-item">
-      <img  className='w123' class="d-block w-100 " src="https://res.cloudinary.com/drhlmb3qr/image/upload/v1736263057/qew6oom5omiqec1lyy29.jpg" alt="Third slide"/>
+    <p>Creating & Production</p>
+       <img className='w123'  class="d-block w-100 " src="https://res.cloudinary.com/drhlmb3qr/image/upload/v1736428907/pexels-gustavo-fring-4173281_lbf7rh.jpg" alt="Second slide"/>
+    </div>
+    <div class="carousel-item">
+    <p>Help you choose your style</p>
+      <img  className='w123' class="d-block w-100 " src="https://res.cloudinary.com/drhlmb3qr/image/upload/v1736430414/pexels-thirdman-8485844_cyf8zx.jpg" alt="Third slide"/>
+    </div>
+    <div class="carousel-item">
+    <p>Online Store</p>
+      <img  className='w123' class="d-block w-100 " src="https://res.cloudinary.com/drhlmb3qr/image/upload/v1736432451/pexels-mart-production-7679456_ssmu0u.jpg" alt="Third slide"/>
+    </div>
+    <div class="carousel-item">
+    <p>On site Store</p>
+      <img  className='w123' class="d-block w-100 " src="https://res.cloudinary.com/drhlmb3qr/image/upload/v1736428827/BERTA_evening___Available_at_our_Soho_showroom_nb2nir.jpg" alt="Third slide"/>
+    </div>
+    <div class="carousel-item">
+    <p>All Of Or Services Available OnLine</p>
+      <img  className='w123' class="d-block w-100 " src="https://res.cloudinary.com/drhlmb3qr/image/upload/v1736432276/pexels-mikhail-nilov-6969967_1_e3munh.jpg" alt="Third slide"/>
     </div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
@@ -24,8 +44,25 @@ const AfterCategory = () => {
     <span class="sr-only">Next</span>
   </a>
 </div>
+<div className='sectionMainPage'>
+   
+    <div>
+        <span><SlBasket /></span>
+    </div>
+    <div>
+        <p>Designing </p>
+        <img src=''/>
+        
+    </div>
+    <div>
+        
+    </div>
+    <div>
+        
+    </div>
 </div>
-  )
+</div>
+)
 }
 
 export default AfterCategory
